0d30d5c71a813e02e7a86e78a5e2ef158638d085
Evan Gyori <evangyori@gmail.com>
2025-01-09 11:10:16 -0800

    Message sent between

--- MyClient.cs
+++ MyClient.cs
@@ -1,3 +1,4 @@
+using System;
 using Steamworks;
 using Steamworks.Data;
 
@@ -7,24 +8,34 @@ public class MyClient : ConnectionManager
 	{
 		base.OnConnected(info);
 		Console.WriteLine("Client OnConnected");
+		Console.WriteLine();
 	}
 
 	public override void OnConnecting(ConnectionInfo info)
 	{
 		base.OnConnecting(info);
 		Console.WriteLine("Client OnConnecting");
+		Console.WriteLine();
 	}
 
 	public override void OnConnectionChanged(ConnectionInfo info)
 	{
 		base.OnConnectionChanged(info);
 		Console.WriteLine($"Client OnConnectionChanged\nState: {info.State}\nidentity: {info.Identity}\nEndReason: {info.EndReason}");
+		Console.WriteLine();
 	}
 
 	public override void OnDisconnected(ConnectionInfo info)
 	{
 		base.OnDisconnected(info);
 		Console.WriteLine("Client OnDisconnected");
+		Console.WriteLine();
 	}
 
+	public override void OnMessage(IntPtr data, int size, long messageNum, long recvTime, int channel)
+	{
+		base.OnMessage(data, size, messageNum, recvTime, channel);
+		Console.WriteLine($"Client OnMessage\nsize: {size}\nmessageNum: {messageNum}\nrecvTime: {recvTime}\nchannel: {channel}");
+		Console.WriteLine();
+	}
 }
--- MyServer.cs
+++ MyServer.cs
@@ -1,29 +1,39 @@
+using System;
 using Steamworks;
 using Steamworks.Data;
 
 public class MyServer : SocketManager
 {
+	Int32 test = 3;
+
 	public override void OnConnected(Connection connection, ConnectionInfo info)
 	{
 		base.OnConnected(connection, info);
 		Console.WriteLine("Server OnConnected");
+		Console.WriteLine();
+
+		IntPtr ptr = new(&test);
+		connection.SendMessage(ptr, 4);
 	}
 
 	public override void OnConnecting(Connection connection, ConnectionInfo info)
 	{
 		base.OnConnecting(connection, info);
 		Console.WriteLine("Server OnConnecting");
+		Console.WriteLine();
 	}
 
 	public override void OnConnectionChanged(Connection connection, ConnectionInfo info)
 	{
 		base.OnConnectionChanged(connection, info);
 		Console.WriteLine($"Server OnConnectionChanged\nState: {info.State}\nidentity: {info.Identity}\nEndReason: {info.EndReason}\ndetailed:\n{connection.DetailedStatus()}");
+		Console.WriteLine();
 	}
 
 	public override void OnDisconnected(Connection connection, ConnectionInfo info)
 	{
 		base.OnDisconnected(connection, info);
 		Console.WriteLine("Server OnDisconnected");
+		Console.WriteLine();
 	}
 }
